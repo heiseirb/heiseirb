3f0c5bd5a89cf0aac1b234e45401e56105ff37e5
Tashiro <g4160hc@gmail.com>
2019-01-10 22:33:37 +0900

    1/17用準備

---
+++ 2019/01/template/kaibun.rb
@@ -0,0 +1,50 @@
+NOONNO*
+回文　ONONO
+NO**NON
+*******
+NO**ON
+NN**N
+O*O*O
+*=O
+# *は必ず一個以上入る
+moziretu =  gets.to_a
+# 回文にできるかどうかの処理
+
+moziretu.size.times do ||
+  if moziretu[i] == moziretu[-i]
+    if moziretu[i]
+  end
+end
+if moziretu.size.odd?
+  # 奇数
+  center_num = moziretu/2
+  moziretu[center_num] = N # Oでも可能
+  zenhan = [] 
+  kouhan = []
+  moziretu/2.times do |i|
+    zenhan << moziretu[i]
+  end
+puts (zenhan + zenhan.reverse).join
+else
+  # 偶数
+  center num = moziretu.lengh/2 #4のとき2
+    num.times do |i|
+      zenhan << moziretu[i]
+    end      
+    moziretu.length - num.times do |i|
+      kouhan << moziretu[]
+  
+end
+# 出来ないの判定
+# 受け取った文字列を配列にする
+# 奇数かつ真ん中が*の場合N or Oを入れる
+# return '出来ません' unless gets.to_a.include?('*')
+
+
+
+ONNO**
+
+t = ONN
+t2 = O**
+t2.reverse # **O
+t = t2.reverse
