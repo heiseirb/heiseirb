3ab9ded6363d1dd28c386d5e796b2643fc0836cf
Tashiro <g4160hc@gmail.com>
2019-01-13 15:30:56 +0900

    kaibunのspecを修正

--- 2019/01/template/spec/kaibun_spec.rb
+++ 2019/01/template/spec/kaibun_spec.rb
@@ -5,17 +5,17 @@
 
 require './kaibun'
 describe "String#to_my_kaibun" do
-  it{ expect( 'geasd'.to_my_kaibun ).to eq('対応してません') }
-  it{ expect( 'NOON'.to_my_kaibun ).to eq('NOON') }
-  it{ expect( 'NO*ON'.to_my_kaibun ).to eq('対応してません') }
-  it{ expect( 'NONN'.to_my_kaibun ).to eq('回文には出来ません') }
-  it{ expect( 'NN'.to_my_kaibun ).to eq('NN') }
-  it{ expect( '**'.to_my_kaibun ).to eq('対応してません') }
-  it{ expect( '*N'.to_my_kaibun ).to eq('NN') }
-  it{ expect( 'NOON**N'.to_my_kaibun ).to eq('NOONOON') }
-  it{ expect( '12341sfdgsdlkjn12'.to_my_kaibun ).to eq('対応してません') }
-  it{ expect( 'NONO*'.to_my_kaibun ).to eq('NONON') }
-  it{ expect( 'N*N*O*'.to_my_kaibun ).to eq('NONNON') }
+  it{ expect( 'geasd'.to_kaibun ).to eq('対応していません') }
+  it{ expect( 'NOON'.to_kaibun ).to eq('NOON') }
+  it{ expect( 'NO*ON'.to_kaibun ).to eq('対応していません') }
+  it{ expect( 'NONN'.to_kaibun ).to eq('回文には出来ません') }
+  it{ expect( 'NN'.to_kaibun ).to eq('NN') }
+  it{ expect( '**'.to_kaibun ).to eq('対応していません') }
+  it{ expect( '*N'.to_kaibun ).to eq('NN') }
+  it{ expect( 'NOON**N'.to_kaibun ).to eq('NOONOON') }
+  it{ expect( '12341sfdgsdlkjn12'.to_kaibun ).to eq('対応していません') }
+  it{ expect( 'NONO*'.to_kaibun ).to eq('NONON') }
+  it{ expect( 'N*N*O*'.to_kaibun ).to eq('NONNON') }
 end
 
 
