44f7672d3e14ba0cbd03fa8f950e011efc2b292d
ayumitamai97 <ayumitamai97@gmail.com>
2019-01-18 00:44:36 +0900

    eto.rb: Replace array with %w()

--- 2019/01/kawauso/eto.rb
+++ 2019/01/kawauso/eto.rb
@@ -2,7 +2,7 @@ class Integer
   def to_eto
     self.validate
     index = self < 4 ? self % 12 + 8 : self % 12 - 4
-    arr = ["子", "丑", "寅", "卯", "辰", "巳", "午", "未", "申", "酉", "戌", "亥"]
+    arr = %w(子 丑 寅 卯 辰 巳 午 未 申 酉 戌 亥)
     arr[index]
   end
 
