3e0f7ebfe7bcfa6f34bfe9e9d321f127b1c19772
ayumitamai97 <ayumitamai97@gmail.com>
2019-01-18 01:23:47 +0900

    Earlier returns

--- 2019/01/kawauso/kaibun.rb
+++ 2019/01/kawauso/kaibun.rb
@@ -1,16 +1,17 @@
 class String
   def to_kaibun
+    return "回文には出来ません" if /NONN/.match(self) # ????
+    return "対応していません" if
+      /^\*+$/.match(self) ||
+      (!/\*/.match(self) && self != self.reverse)
+    if self.length % 2 == 1
+      return "対応していません" if self.chars.to_a[self.length / 2] == "*"
+    end
+    return self if self == self.reverse
+
     index = self.index("*")
     count = self.scan(/\*/).count
-    replacer = self.reverse.split("")[index..(index+count-1)].join("")
+    replacer = self.reverse.chars.to_a[index..(index + count - 1)].join("")
     self.sub((self.scan(/\*/).join("")), replacer)
   end
-  # def to_kaibun
-  #   # self.chars.to_a.count % 2 == 0 ? self.even : self.od
-  #   self.even
-  # end
-  # def even
-  #   arr = self.chars.to_a
-  #   (arr[0..arr.length / 2] + arr[0..arr.length / 2].reverse).join("")
-  # end
 end
