b29a99a31cc8c1a90d0401b6c9290490b35aee32
Natsukingdom <natsuki.m.1992@gmail.com>
2019-01-17 21:00:24 +0900

    リファクタ

--- 2019/01/torasan_team/eto.rb
+++ 2019/01/torasan_team/eto.rb
@@ -2,9 +2,9 @@
 
 class Integer
   # 西暦を与えて十二支を返すメソッド
+  ETOS = %w[申 酉 戌 亥 子 丑 寅 卯 辰 巳 午 未].freeze
   def to_eto
     raise '負の値です．' if self < 0
-    etos = %w[申 酉 戌 亥 子 丑 寅 卯 辰 巳 午 未]
-    return etos[self % 12]
+    return ETOS[self % ETOS.size]
   end
 end
