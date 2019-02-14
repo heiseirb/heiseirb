0144d007a9a41e4e6c6f756ed3c069477218da07
haze-it <shoou0000@gmail.com>
2019-02-14 20:30:31 +0900

    fix

--- 2019/02/migihaji/to_biggest_num.rb
+++ 2019/02/migihaji/to_biggest_num.rb
@@ -1,6 +1,10 @@
 class Array
   def to_biggest_num
-    raise NecessaryValueError.new unless self.all? { |n| n.is_a?(Integer) }
+    raise NecessaryValueError.new unless self.all? { |n| n.is_a?(Numeric) && n >= 0 }
+
+    chars = self.map(&:to_s)
+    sorted_char = chars.sort_by {|char| char[0]}.reverse
+    sorted_char.join('').to_i
   end
 end
 
