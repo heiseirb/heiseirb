558b65548f4ac8477e1b162fb04fe5fe1e1777c7
ayumitamai97 <ayumitamai97@gmail.com>
2019-02-14 20:42:28 +0900

    Extract indicated string range

--- 2019/02/template/has_love.rb
+++ 2019/02/template/has_love.rb
@@ -1,6 +1,16 @@
+require "pry"
+
 class HasLove
-  def initialize
+  def initialize(sentence:)
+    @arr = sentence.split("\n").map(&:chars)
   end
-  def find
+  def find(start_col, end_col, start_row, end_row)
+    start_col -= 1
+    end_col -= 1
+    start_row -= 1
+    end_row -= 1
+    rows = @arr[start_row..end_row]
+    rows.map{|row| row[start_col..end_col]}
+    binding.pry
   end
 end
