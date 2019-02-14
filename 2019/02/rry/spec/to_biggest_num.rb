3b81b85632da4c4f6d95059fe34eeccb6c20b305
ryamakuchi <r.yamakuchi@gmail.com>
2019-02-14 21:35:42 +0900

    Spec 通るコード

--- 2019/02/rry/spec/to_biggest_num.rb
+++ 2019/02/rry/spec/to_biggest_num.rb
@@ -6,7 +6,7 @@ describe "Array#to_biggest_num" do
   it { expect{ [50, 2, [123], 3].to_biggest_num }.to raise_error(NecessaryValueError) }
   it { expect{ [50, {key: 'value'}, 123, 3].to_biggest_num }.to raise_error(NecessaryValueError) }
   it { expect([0, 0, 0, 9].to_biggest_num).to eq(9000) }
-  it { expect([9, 3000, 9, 9].to_biggest_num).to eq(3000999) }
+  it { expect([9, 3000, 9, 9].to_biggest_num).to eq(9993000) }
   it { expect([0, 0.001, 9, 3].to_biggest_num).to eq(9300.001) }
   it { expect{ [0, -0.001, 9, 3].to_biggest_num }.to raise_error(NecessaryValueError) }
 end
--- 2019/02/rry/to_biggest_num.rb
+++ 2019/02/rry/to_biggest_num.rb
@@ -1,4 +1,39 @@
 class Array
   def to_biggest_num
+    raise NecessaryValueError unless only_greater_than_or_equal_to_zero_integer?
+    float = self.select{|i| i.class == Float }
+    self.delete(float[0]) unless (float.empty?)
+    joined = self.map{|i| [i.to_s[0,1],i.to_s]}.sort.reverse.map{|i| i[1]}.join
+    joined = joined + float[0].to_s unless (float.empty?)
+    return joined.to_f unless (float.empty?)
+    joined.to_i
+  end
+
+  private
+
+  def only_greater_than_or_equal_to_zero_integer?
+    !include_string? && !include_hash? && !include_array? && !include_less_than_zero_integer?
+  end
+
+  def include_string?
+    select { |element| element.class == String }.size != 0
+  end
+
+  def include_hash?
+    select { |element| element.class == Hash }.size != 0
+  end
+
+  def include_array?
+    select { |element| element.class == Array }.size != 0
+  end
+
+  def include_less_than_zero_integer?
+    reject { |element| element >= 0 }.size != 0
+  end
+
+  def include_float?
+    select { |element| element.class == Float }.size != 0
   end
 end
+
+class NecessaryValueError < StandardError; end
