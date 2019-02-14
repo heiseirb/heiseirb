40bb13e696b350693b582f487bb03b7628d131a5
asayamakk <akkdkb@gmail.com>
2019-02-14 20:49:27 +0900

    エラーのケースをはじく

--- 2019/02/migihaji/has_love.rb
+++ 2019/02/migihaji/has_love.rb
@@ -1,10 +1,23 @@
+class OutOfRangeError < StandardError
+
+end
+
+class InvalidRangeError < StandardError
+
+end
+
 class HasLove
   def initialize(sentence: nil)
     @str = sentence
     @lines = @str.lines(chomp: true)
     @chars = @lines.map{ |c| c.each_char.to_a }
   end
-  def find
-    @chars
+
+  def find(top, bottom, left, right)
+    raise OutOfRangeError if left > 20 || right > 20 || top > 10 || bottom > 10
+    raise InvalidRangeError if top > bottom || left > right
+
+   pp @chars
+
   end
 end
