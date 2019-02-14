47732eacbfde95f15a96a6d45828cc3efb289997
haze-it <shoou0000@gmail.com>
2019-02-14 20:39:29 +0900

    [wip] fix has_love

--- 2019/02/migihaji/has_love.rb
+++ 2019/02/migihaji/has_love.rb
@@ -1,6 +1,10 @@
 class HasLove
-  def initialize
+  def initialize(sentence: nil)
+    @str = sentence
+    @lines = @str.lines(chomp: true)
+    @chars = @lines.map{ |c| c.each_char.to_a }
   end
   def find
+    @chars
   end
 end
