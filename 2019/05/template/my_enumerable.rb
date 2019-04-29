ea5033dcf5ed65eb48571a67079d8df3f3f8144a
Tashiro <g4160hc@gmail.com>
2019-04-29 10:09:14 +0900

    arg追加

--- 2019/05/template/my_enumerable.rb
+++ 2019/05/template/my_enumerable.rb
@@ -1,13 +1,13 @@
 module Enumerable
   def my_each; end
 
-  def my_find; end
+  def my_find(arg); end
 
-  def my_map; end
+  def my_map(arg); end
 
   def my_each_with_object(arg); end
 
-  def my_group_by; end
+  def my_group_by(arg); end
 
   def tally; end
 end
