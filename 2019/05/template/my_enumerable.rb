bd5987539c0db7503bb759824ad12546a3812896
cobafan <akitaka0405@gmail.com>
2019-05-16 21:34:37 +0900

    aaaaaaaaaaaa

--- 2019/05/template/my_enumerable.rb
+++ 2019/05/template/my_enumerable.rb
@@ -2,7 +2,7 @@ require 'byebug'
 module Enumerable
   # eachを独自実装しよう！！！
   # forとかwhileでやってみてね。
-  def my_each(&block)
+  def my_each
     index = 0
     while self.size > index do
       yield self[index]
@@ -12,7 +12,11 @@ module Enumerable
   end
 
   # 下記問題、eachの使用は禁止。my_eachとかreduceはいいよん
-  def my_find(arg); end
+  def my_find(arg = nil, &block)
+    self.my_each do |element|
+      yield element
+    end
+  end
 
   def my_map(arg); end
 
--- 2019/05/template/my_enumerable_test.rb
+++ 2019/05/template/my_enumerable_test.rb
@@ -13,7 +13,6 @@ class MyEnumerableTest < MiniTest::Test
 
   # https://ref.xaio.jp/ruby/classes/enumerable/find
   def test_find
-    skip
     assert_equal (1..100).find { |i| i % 5 == 0 && i % 7 == 0 }, (1..100).my_find { |i| i % 5 == 0 && i % 7 == 0 }
     not_found = -> { 'NOT FOUND' }
     assert_equal (1..100).find(not_found) { |i| i % 5 == 0 && i % 7 == 0 }, (1..100).my_find(not_found) { |i| i % 5 == 0 && i % 7 == 0 }
