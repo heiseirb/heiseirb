0a531566530de11e8050a5d0860ba3c797981f73
yuko yuri <yuriko11d@gmail.com>
2019-05-16 20:16:51 +0900

    new files

---
+++ 2019/05/gumi/my_enumerable.rb
@@ -0,0 +1,20 @@
+module Enumerable
+  # eachを独自実装しよう！！！
+  # forとかwhileでやってみてね。
+  def my_each; end
+
+  # 下記問題、eachの使用は禁止。my_eachとかreduceはいいよん
+  def my_find(arg); end
+
+  def my_map(arg); end
+
+  def my_each_with_object(arg); end
+
+  def my_group_by(arg); end
+
+  def tally; end
+end
+
+class Array
+  def my_zip(*args); end
+end
