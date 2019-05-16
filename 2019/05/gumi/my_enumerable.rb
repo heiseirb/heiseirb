906ee182775d7f2b0c0e2d7e3f61f68e0633f5b6
ayumitamai97 <ayumitamai97@gmail.com>
2019-05-16 20:36:36 +0900

    Proc.newしてみた

--- 2019/05/gumi/my_enumerable.rb
+++ 2019/05/gumi/my_enumerable.rb
@@ -1,7 +1,11 @@
+require 'pry'
+
 module Enumerable
   # eachを独自実装しよう！！！
   # forとかwhileでやってみてね。
-  def my_each; end
+  def my_each(*args)
+    binding.pry
+  end
 
   # 下記問題、eachの使用は禁止。my_eachとかreduceはいいよん
   def my_find(arg); end
@@ -16,5 +20,8 @@ module Enumerable
 end
 
 class Array
-  def my_zip(*args); end
+  def my_zip(*args, &block)
+  end
 end
+
+[0, 1, 2].my_each(Proc.new { |e| e * 3 })
