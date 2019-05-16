16f05bdf9eee743a2d9de1d1f9c0d231d46dc619
ayumitamai97 <ayumitamai97@gmail.com>
2019-05-16 20:48:37 +0900

    Completed my_each

--- 2019/05/gumi/my_enumerable.rb
+++ 2019/05/gumi/my_enumerable.rb
@@ -3,20 +3,28 @@ require 'pry'
 module Enumerable
   # eachを独自実装しよう！！！
   # forとかwhileでやってみてね。
-  def my_each(*args)
-    binding.pry
+  def my_each(&block)
+    for e in self
+      yield(e)
+    end
   end
 
   # 下記問題、eachの使用は禁止。my_eachとかreduceはいいよん
-  def my_find(arg); end
+  def my_find(arg)
+  end
 
-  def my_map(arg); end
+  def my_map(&block)
+    binding.pry
+  end
 
-  def my_each_with_object(arg); end
+  def my_each_with_object(arg)
+  end
 
-  def my_group_by(arg); end
+  def my_group_by(arg)
+  end
 
-  def tally; end
+  def tally
+  end
 end
 
 class Array
@@ -24,4 +32,4 @@ class Array
   end
 end
 
-[0, 1, 2].my_each(Proc.new { |e| e * 3 })
+[1, 3, 2].my_map { |e| p e }
