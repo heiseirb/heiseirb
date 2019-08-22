25780805d5b3f014915e2efd5e1d028e3feaeac9
cobafan <akitaka0405@gmail.com>
2019-08-22 21:24:17 +0900

    tern end

--- 2019/08/template/heiseirb_api_client/spec/heiseirb_api_client_spec.rb
+++ 2019/08/template/heiseirb_api_client/spec/heiseirb_api_client_spec.rb
@@ -4,28 +4,16 @@ RSpec.describe HeiseirbApiClient do
     expect(HeiseirbApiClient::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-
   context '#similar_users' do
-    let(:user_id) { User.first.id }
-    # 1. 該当ユーザのイベントスケジュールを全部取ってくる
-    # 2. 1件目のイベントに登録しているユーザ全部引く(自身は除く)
-    # 3. 2件目のイベントに登録しているユーザを取ってきて
-    #
+    let(:user_id) { 1 }
+
     subject do
       client = HeiseirbApiClient::SimilarUser.new
       client.similar_users(user_id)
     end
     it do
       subject
+      expect(subject).to eq([95, 38, 77])
     end
   end
-
-
-
-
-
-
 end
