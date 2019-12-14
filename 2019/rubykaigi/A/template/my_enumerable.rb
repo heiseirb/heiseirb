a3713ed218cf0644e9a0057f5ca70b01af0a5ce8
Kosei IDE <kos31de@gmail.com>
2019-12-14 16:21:32 +0900

    enumerableの再実装を追加

--- 2019/rubykaigi/A/template/my_enumerable.rb
+++ 2019/rubykaigi/A/template/my_enumerable.rb
@@ -1,12 +1,34 @@
 module Enumerable
   # eachを独自実装しよう！！！
   # forとかwhileでやってみてね。
-  def my_each; end
+  def my_each
+    for i in 0...self.length
+      yield self[i]
+    end
+  end
 
   # 下記問題、eachの使用は禁止。my_eachとかreduceはいいよん
-  def my_find(arg); end
+  def my_find(arg=nil)
+    for item in self
+      if yield item
+        return item
+      end
+    end
+    
+    if arg
+      arg.call
+    else
+      nil
+    end
+  end
 
-  def my_map(arg); end
+  def my_map(arg=nil)
+    result = []
+    for item in self
+      result << yield(item)
+    end
+    result
+  end
 
   def my_each_with_object(arg); end
 
