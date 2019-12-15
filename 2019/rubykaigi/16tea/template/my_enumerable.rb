e43738d5136765e6c529b8ef330261127a96578c
pyons <broad.basic15@gmail.com>
2019-12-15 11:42:30 +0900

    implement my_each mehtod.

--- 2019/rubykaigi/16tea/template/my_enumerable.rb
+++ 2019/rubykaigi/16tea/template/my_enumerable.rb
@@ -1,7 +1,13 @@
 module Enumerable
   # eachを独自実装しよう！！！
   # forとかwhileでやってみてね。
-  def my_each; end
+  def my_each
+    tmp = []
+    for i in 0..self.size-1 do 
+      tmp << yield(self[i]) 
+    end
+    return tmp
+  end
 
   # 下記問題、eachの使用は禁止。my_eachとかreduceはいいよん
   def my_find(arg); end
